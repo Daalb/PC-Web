2f979ed1a38c788f76be73765674a436512dd66f
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2020-06-16 16:25:52 -0500

    Nothing to commit

--- pc-client/src/layouts/LayoutBasic.js
+++ pc-client/src/layouts/LayoutBasic.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import {Layout} from 'antd';
 import {Route} from 'react-router-dom';
+import "./LayoutBasic.scss";
+
 
 export default function LayoutBasic({routes}){
-    const { Header,Content,Footer } = Layout; 
+    const { Content,Footer } = Layout; 
 
     return(
         <Layout>
-            <h2>Menu Sider Basic User</h2>
+            <h2>Menu...</h2>
             <Layout>
-                <Header>Menu Sider Basic User</Header>
                 <Content>
-                    <LoadRoutes routes={routes}/>
+                    <LoadRoutes routes={routes}/>   
                 </Content>
                 <Footer>
                     Partners Campus Uninorte
