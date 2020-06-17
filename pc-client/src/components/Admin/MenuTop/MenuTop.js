a604f4975701a8d1b346fa1e990813c613b6d652
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2020-06-17 16:07:31 -0500

    SCSS added, working in admin design

---
+++ pc-client/src/components/Admin/MenuTop/MenuTop.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button } from 'antd';
+import { MenuUnfoldOutlined } from '@ant-design/icons';
+import './MenuTop.scss';
+import PCLogo from '../../../assets/img/png/logo-pcuninorte.png'
+
+
+export default function MenuTop(){
+    return (
+        <div className="menu-top">
+            <div className="menu-top__left">
+                <img 
+                    className="menu-top__left-logo"
+                     src={PCLogo}
+                     alt="Logo Partners Campus Uninorte"
+                />
+                <Button type="link" onClick={()=>console.log('Click hecho')}>
+                    <MenuUnfoldOutlined />
+                </Button>
+            </div>
+        </div>
+    )
+}
--- pc-client/src/layouts/LayoutAdmin.js
+++ pc-client/src/layouts/LayoutAdmin.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Route,Switch} from 'react-router-dom';
 import {Layout} from "antd";//Nos traemos un layout de la libreria de css antd
+import MenuTop from '../components/Admin/MenuTop';
+
 import "./LayoutAdmin.scss";
 
 export default function LayoutAdmin(props){
@@ -8,13 +10,15 @@ export default function LayoutAdmin(props){
     const { Header,Content,Footer } = Layout; 
     return(
         <Layout>
-            <h2>Menu Sider Admin</h2>
-            <Layout>
-                <Header>Header...</Header>
-                <Content>
+            {/*TO DO :Menu Sider*/}
+            <Layout className="layout-admin">
+                <Header className="layout-admin__header">
+                    <MenuTop/>
+                </Header>
+                <Content className="layout-admin__content">
                     <LoadRoutes routes={routes}/>
                 </Content>
-                <Footer>
+                <Footer className="layout-admin__footer">
                     Partners Campus Uninorte
                 </Footer>
             </Layout>
