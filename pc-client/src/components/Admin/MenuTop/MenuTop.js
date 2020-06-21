cce0956dbe9c34e8db781b63ac567c2570039fbd
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2020-06-21 15:58:35 -0500

    Menu sider for admin

--- pc-client/src/components/Admin/MenuTop/MenuTop.js
+++ pc-client/src/components/Admin/MenuTop/MenuTop.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { Button } from 'antd';
-import { MenuUnfoldOutlined,PoweroffOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import { MenuUnfoldOutlined,PoweroffOutlined,MenuFoldOutlined } from '@ant-design/icons';
 import './MenuTop.scss';
 import PCLogo from '../../../assets/img/png/logo-pcuninorte.png'
 
 
-export default function MenuTop(){
+export default function MenuTop(props){
+    console.log(props); 
+    const {menuCollapsed, setMenuCollapsed} = props;
     return (
         <div className="menu-top">
             <div className="menu-top__left">
-                <img 
-                    className="menu-top__left-logo"
-                     src={PCLogo}
-                     alt="Logo Partners Campus Uninorte"
-                />
-                <Button type="link" onClick={()=>console.log('Click hecho')}>
-                    <MenuUnfoldOutlined />
+                <Link to = "/admin">
+                    <img 
+                        className="menu-top__left-logo"
+                        src={PCLogo}
+                        alt="Logo Partners Campus Uninorte"
+                        
+                    />
+                </Link>
+                <Button type="link" onClick={() => setMenuCollapsed(!menuCollapsed)}>
+                    {menuCollapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
                 </Button>
             </div>
             <div className="menu-top__right">
