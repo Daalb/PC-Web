a266d70bb3576945c8ca449d05c76b717b7838a5
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2020-06-22 18:37:25 -0500

    SignUp controlle and DB

--- pc-server/controllers/user.js
+++ pc-server/controllers/user.js
@@ -2,7 +2,45 @@ const bcrypt = require("bcrypt-nodejs");
 const User = require("../models/user");
 
 function signUp(req, res){
-    console.log('Endpoint Ejecutado');
+    const user = new User();
+
+    const{name, lastName, email, password, repeatPassword} = req.body;
+
+    //Asignaciones al modelo
+    user.name = name;
+    user.lastName = lastName; 
+    user.email = email;
+    user.role = "admin";
+    user.active = false;
+
+    if (!password || !repeatPassword){
+        res.status(404).send({ message: "Las contraseñas son obligatorias."});
+    } else {
+        if (password !== repeatPassword){
+            res.status(404).send({ message: "Las contraseñas tienen que ser iguales. "});
+        } else{
+            bcrypt.hash(password, null, null, function(err, hash){
+                if (err) {
+                    res.status(500).send({message: "Error al encriptar la contraseña"});
+                }else{
+                    //res.status(200).send({message: hash});
+                    user.password = hash;//Asignación de la encriptación a el atributo password
+
+                    user.save((err,userStored)=>{
+                        if (err) {
+                            res.status(500).send({message: "Error del servidor" + err});
+                        } else {
+                            if(!userStored) {
+                                res.status(404).send({message: "Error al crear el usuario"});
+                            } else{
+                                res.status(200).send({user: userStored});
+                            }
+                        }
+                    });
+                }
+            });
+        }
+    }
 }
 
 module.exports = {
--- pc-server/models/user.js
+++ pc-server/models/user.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = Schema({
-    nombre: String,
-    lastname: String,
+    name: String,
+    lastName: String,
     email:{
         type: String,
         unique: true
