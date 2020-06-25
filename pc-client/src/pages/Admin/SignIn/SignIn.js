5ffb04e5e96e7851025f4916145c8779f6316acd
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2020-06-24 20:14:25 -0500

    Tabs to login and register

--- pc-client/src/pages/Admin/SignIn/SignIn.js
+++ pc-client/src/pages/Admin/SignIn/SignIn.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Layout, Tabs} from 'antd';
+import { Redirect } from 'react-router-dom';
+import Logo from '../../../assets/img/png/logo-pcuninorte.png';
+
 import './SignIn.scss';
+
+
 export default function SignIn(){
+    const { Content } = Layout;
+    const { TabPane } = Tabs;
     return(
-        <div>
-            <h1>Estamos en SignIn</h1>
-        </div>
+        <Layout className ="sign-in">
+            <Content className= "sign-in__content">
+                <h1 className= "sign-in__content__logo">
+                    <img src={Logo} alt="Logo Parterns Campus " className = "sign-in__content__logo__img"/>
+                </h1>
+                <div className="sign-in__content-tabs">
+                    <Tabs type ="card">
+                        <TabPane tab={<span>Entrar</span>} key="1">
+                            Componente LoginForm
+                        </TabPane>
+                        
+                        <TabPane tab={<span>Nuevo Usuario</span>} key="2">
+                            Componente RegisterForm
+                        </TabPane>
+
+                    </Tabs>
+                </div>
+            </Content>
+        </Layout>
     );
 }
