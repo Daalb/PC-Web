2224dfa6cb4136a1c468887bb29cf8a92d019cc1
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2020-07-04 23:23:53 -0500

    Headers fixed

--- pc-server/routes/user.js
+++ pc-server/routes/user.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const UserController = require("../controllers/user");
+const md_auth = require("../middleware/authenticated");
 
 const api = express.Router();
 
 api.post("/sign-up",UserController.signUp);//cuando se haga POST a la ruta se ejecuta la función 
 api.post("/sign-in",UserController.singIn);
+api.get("/users",[md_auth.ensureAuth],UserController.getUsers);
 
 module.exports = api;
