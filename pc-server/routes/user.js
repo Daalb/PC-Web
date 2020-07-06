a3abab7d8333ad0e1d18fa7077e81394e400dd30
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2020-07-05 20:28:54 -0500

    Active/Inactive Users

--- pc-server/routes/user.js
+++ pc-server/routes/user.js
@@ -7,5 +7,7 @@ const api = express.Router();
 api.post("/sign-up",UserController.signUp);//cuando se haga POST a la ruta se ejecuta la función 
 api.post("/sign-in",UserController.singIn);
 api.get("/users",[md_auth.ensureAuth],UserController.getUsers);
+api.get("/users-active",[md_auth.ensureAuth],UserController.getUsersActive);
+
 
 module.exports = api;
