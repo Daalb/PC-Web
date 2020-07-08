f81cc3a3b835e7e031174d216ad640851e0ec1d8
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2020-07-08 00:08:43 -0500

    Form finished

--- pc-client/src/components/Admin/Users/EditUserForm/EditUserForm.js
+++ pc-client/src/components/Admin/Users/EditUserForm/EditUserForm.js
@@ -1,7 +1,7 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { Avatar, Form, Input, Select, Button, Row, Col } from 'antd';
-import { UserOutlined, MailOutlined } from '@ant-design/icons';
+import { UserOutlined, MailOutlined, PhoneOutlined, FileOutlined } from '@ant-design/icons';
 
 import NoAvatar from '../../../../assets/img/png/no-avatar.png';
 
@@ -23,6 +23,13 @@ export default function EditUserForm(props){
 
 
     });
+
+    useEffect(()=> {
+        if(avatar) {
+            setUserData({...userData,avatar })
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[avatar]);
 
     const updateUser = e => {
         console.log(userData);
@@ -83,30 +90,86 @@ function EditForm(props){
                     </Form.Item>
                 </Col>
                 <Col span={12}>
-                <Input
-                    prefix= {<UserOutlined />}
-                    placeholder="Apellidos"
-                    defaultValue={userData.lastName}
-                    onChange={e => setUserData({...userData, lastName: e.target.value})}
-                />
+                    <Form.Item>
+                        <Input
+                            prefix= {<UserOutlined />}
+                            placeholder="Apellidos"
+                            defaultValue={userData.lastName}
+                            onChange={e => setUserData({...userData, lastName: e.target.value})}
+                        />
+                    </Form.Item>
                 </Col>
             </Row>
 
             <Row gutter={24}>
                 <Col span={12}>
-                    <Input
-                        prefix= {<MailOutlined />}
-                        placeholder="Corre electronico"
-                        defaultValue={userData.email}
-                        onChange={e => setUserData({...userData, email: e.target.value})}
-                    />
+                    <Form.Item>
+                        <Input
+                            prefix= {<MailOutlined />}
+                            placeholder="Corre electronico"
+                            defaultValue={userData.email}
+                            onChange={e => setUserData({...userData, email: e.target.value})}
+                        />
+                    </Form.Item>
                 </Col>
-                <Col span={12}></Col>
+                <Col span={12}>
+                    <Form.Item>
+                        <Input
+                            prefix= {<PhoneOutlined />}
+                            placeholder="Teléfono"
+                            defaultValue={userData.phone}
+                            onChange={e => setUserData({...userData, phone: e.target.value})}
+                        />
+                    </Form.Item>
+                </Col>
             </Row>
 
             <Row gutter={24}>
-                <Col span={12}></Col>
-                <Col span={12}></Col>
+                <Col span={12}>
+                    <Form.Item>
+                        <Input
+                            prefix= {<FileOutlined />}
+                            placeholder="Carrera"
+                            defaultValue={userData.carrera}
+                            onChange={e => setUserData({...userData, carrera: e.target.value})}
+                        />
+                    </Form.Item>
+                </Col>
+                <Col span={12}>
+                    <Form.Item>
+                        <Select
+                            placeholder="Selecciona un rol"
+                            onChange={ e => setUserData({...userData, role: e}) }
+                            defaultValue={userData.role}
+                        >
+                            <Option value="admin"> Administrador </Option>
+                        </Select>
+                    </Form.Item>      
+                </Col>
+            </Row>
+
+            <Row gutter={24}>
+                <Col span={12}>
+                    <Form.Item>
+                        <Input
+                            prefix= {<FileOutlined />}
+                            placeholder="Contraseña"
+                            defaultValue={userData.password}
+                            onChange={e => setUserData({...userData, carrera: e.target.value})}
+                        />
+                    </Form.Item>
+                </Col>
+                <Col span={12}>
+                    <Form.Item>
+                        <Select
+                            placeholder="Selecciona un rol"
+                            onChange={ e => setUserData({...userData, role: e}) }
+                            defaultValue={userData.role}
+                        >
+                            <Option value="admin"> Administrador </Option>
+                        </Select>
+                    </Form.Item>      
+                </Col>
             </Row>
 
             <Form.Item>
