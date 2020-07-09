ce8efe02659bc3020dd1a7968888b6e6ab81707b
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2020-07-09 18:54:31 -0500

    Get image of the avatar

--- pc-server/routes/user.js
+++ pc-server/routes/user.js
@@ -13,6 +13,7 @@ api.post("/sign-in",UserController.singIn);
 api.get("/users",[md_auth.ensureAuth],UserController.getUsers);
 api.get("/users-active",[md_auth.ensureAuth],UserController.getUsersActive);
 api.put("/upload-avatar/:id",[md_auth.ensureAuth, md_upload_avatar],UserController.uploadAvatar);
+api.get("/get-avatar/:avatarName",UserController.getAvatar);//Cualquier usuario puede ver el avatar aunque no esté logeado
 
 
 module.exports = api;
