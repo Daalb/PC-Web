6a248632362b2d3db01be2eba135e6feb918a93e
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2020-07-09 18:37:47 -0500

    Endpoint to upload avatar finished

--- pc-server/routes/user.js
+++ pc-server/routes/user.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const multipart = require("connect-multiparty");
 const UserController = require("../controllers/user");
+
+//Middlewares
 const md_auth = require("../middleware/authenticated");
+const md_upload_avatar = multipart({uploadDir:"./uploads/avatar"}); //Arrastra la imagen a la carpeta uploads/avatar
 
 const api = express.Router();
 
@@ -8,6 +12,7 @@ api.post("/sign-up",UserController.signUp);//cuando se haga POST a la ruta se ej
 api.post("/sign-in",UserController.singIn);
 api.get("/users",[md_auth.ensureAuth],UserController.getUsers);
 api.get("/users-active",[md_auth.ensureAuth],UserController.getUsersActive);
+api.put("/upload-avatar/:id",[md_auth.ensureAuth, md_upload_avatar],UserController.uploadAvatar);
 
 
 module.exports = api;
