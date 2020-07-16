b3e9a23da7695cf2a8d46d434517dd604af40aed
Diego Antonio Albor Rojas <30907931+Daalb@users.noreply.github.com>
2020-07-16 14:12:51 -0500

    Working in events

---
+++ pc-client/src/components/Admin/Events/Events.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Events(){
+    return(
+        <div>
+            <h1>Gestión de reuniones...</h1>
+        </div>
+
+    );
+}
--- pc-client/src/pages/Admin/Attendance/Attendance.js
+++ pc-client/src/pages/Admin/Attendance/Attendance.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Events from '../../../components/Admin/Events/Events';
+
 
 export default function Attendance(){
     return(
         <div>
-            <h1>Attendance...</h1>
+            <Events/>
         </div>
     );
 }
