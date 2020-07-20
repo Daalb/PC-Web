45c259e487234fea5ce2644dbb85f74e5c42ab7c
lpotte <lpotte@uninorte.edu.co>
2020-07-20 12:39:39 -0500

    Primera parte del front de eventos

--- pc-client/src/components/Admin/Events/Events.js
+++ pc-client/src/components/Admin/Events/Events.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import { List, Card, Button, PageHeader} from 'antd';
+import './Events.scss';
+
+const data = [
+    {
+    title: 'Reunión 1',
+    },
+    {
+    title: 'Reunión 2',
+    },
+    {
+    title: 'Reunión 3',
+    },
+    {
+    title: 'Reunión 4',
+    },
+  ];
 
 export default function Events(){
     return(
-        <div>
-            <h1>Gestión de reuniones...</h1>
+      <main>
+        <PageHeader
+          title={
+            "Registros de las reuniones realizadas"
+          }
+          extra={[
+            <Button key="2" type="primary">Registrar Reunión</Button>,
+            <Button key="1" type="primary">Editar Reunión</Button>,
+          ]}
+        />
+        <div class="Grid">
+          <List
+              grid={{ gutter: 20, column: 4, justify: "center", align:"middle" }}
+              pagination={{
+                onChange: page => {
+                  console.log(page);
+                },
+                pageSize: 8,
+                responsive: true,
+              }}
+              dataSource={data}
+              renderItem={item => (
+                  <List.Item>
+                      <Card title={item.title}> 
+                          <p>Descripcion de la Reunión</p>
+                      </Card>
+                  </List.Item>
+              )}
+            />
         </div>
-
-    );
-}
+      </main>  
+    )
+};
