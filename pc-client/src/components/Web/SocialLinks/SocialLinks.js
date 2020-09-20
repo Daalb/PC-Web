227bf8aeddcf46b4b0cbc0137cd6c6c4fe0f1d79
Daalb <daalbor@uninorte.edu.co>
2020-09-19 22:15:53 -0500

    Adding icons

--- pc-client/src/components/Web/SocialLinks/SocialLinks.js
+++ pc-client/src/components/Web/SocialLinks/SocialLinks.js
@@ -2,9 +2,21 @@
 import React from "react";
 
 
+//Estilos
+import {ReactComponent as FacebookIcon} from "../../../assets/img/svg/facebook.svg";
+import "./SocialLinks.scss"
+
 export default function SocialLinks(){
     return(
-        <h1>Social SocialLinks</h1>
+        <div className="social-links">
+            <a 
+                href="https://www.facebook.com/PCUninorte"
+                className="facebook"
+                target="_blank"
+            >
+                <FacebookIcon/>
+            </a>
+        </div>
     )
 }
 
