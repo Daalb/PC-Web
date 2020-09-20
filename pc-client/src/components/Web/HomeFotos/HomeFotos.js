eb7c11f866482e35fc6b1e3fcf4313ed68ddc561
Daalb <daalbor@uninorte.edu.co>
2020-09-20 11:01:02 -0500

    Working in styles

--- pc-client/src/components/Web/HomeFotos/HomeFotos.js
+++ pc-client/src/components/Web/HomeFotos/HomeFotos.js
@@ -22,17 +22,59 @@ export default function HomeFotos() {
           <Col lg={4}/>
           <Col lg={16}>
             <Row className="row-fotos">
-                <Col md={6}>Foto...</Col>
-                <Col md={6}>Foto...</Col>
-                <Col md={6}>Foto...</Col>
-                <Col md={6}>Foto...</Col>
+                <Col md={6}>
+                    <CardFoto 
+                        image={foto01}
+                        title="Titulo 1"
+                        subtitle="Subtitulo1"
+                    />
+                </Col>
+
+                <Col md={6}>
+                    <CardFoto 
+                        image={foto02}
+                        title="Titulo 2"
+                        subtitle="Subtitulo 2"
+                    />
+                </Col>
+
+                <Col md={6}>
+                    <CardFoto 
+                        image={foto03}
+                        title="Titulo 3"
+                        subtitle="Subtitulo3"
+                    />
+                </Col>
+
+                <Col md={6}>
+                    <CardFoto 
+                        image={foto04}
+                        title="Titulo 4"
+                        subtitle="Subtitulo4"
+                    />
+                </Col>
+                
             </Row>
           </Col>
           <Col lg={4}/>
-
-
-
-
       </Row>
+    )
+}
+
+function CardFoto(props){
+    const {image, title, subtitle, link} = props;
+    const{ Meta } = Card;
+
+    return(
+        <a href={link} target="_blank" >
+            <Card
+                className="home-fotos__card"
+                cover={<img src={image} alt={title} />}
+                actions={[<Button>Ingresar</Button>]}
+            >
+                <Meta title={title} description={subtitle}/>
+            </Card>
+
+        </a>
     )
 }
