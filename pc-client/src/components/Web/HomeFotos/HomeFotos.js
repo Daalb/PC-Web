7926fe879ca5de0a72d0287c8b109ebbbba030ef
Daalb <daalbor@uninorte.edu.co>
2020-09-20 10:45:56 -0500

    Adding fotos

--- pc-client/src/components/Web/HomeFotos/HomeFotos.js
+++ pc-client/src/components/Web/HomeFotos/HomeFotos.js
@@ -1,10 +1,38 @@
 //Liberías
-import React from 'react'
+import React from 'react';
+import {Row, Col, Card, Button} from 'antd';
+import {Link} from 'react-router-dom';
+
+//Imagenes
+import foto01 from "../../../assets/img/jpg/foto01.jpg";
+import foto02 from "../../../assets/img/jpg/foto02.jpg"
+import foto03 from "../../../assets/img/jpg/foto03.jpg"
+import foto04 from "../../../assets/img/jpg/foto04.jpg"
+
+//Estilos
+import "./HomeFotos.scss";
 
 export default function HomeFotos() {
     return (
-        <div>
-            
-        </div>
+      <Row className="home-fotos">
+          <Col lg={24} className="home-fotos__title">
+              <h2>Echa un vistazo a algunas de nuestras actividades</h2>
+          </Col>
+          
+          <Col lg={4}/>
+          <Col lg={16}>
+            <Row className="row-fotos">
+                <Col md={6}>Foto...</Col>
+                <Col md={6}>Foto...</Col>
+                <Col md={6}>Foto...</Col>
+                <Col md={6}>Foto...</Col>
+            </Row>
+          </Col>
+          <Col lg={4}/>
+
+
+
+
+      </Row>
     )
 }
--- pc-client/src/pages/Home.js
+++ pc-client/src/pages/Home.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 //Componentes
 import MainBanner from "../components/Web/MainBanner";
+import HomeFotos from "../components/Web/HomeFotos";
 
 export default function Home(){
 
     return(
-        <MainBanner/>
-        
+        <>
+            <MainBanner/>
+            <HomeFotos/>
+        </>
 
     );
 }
