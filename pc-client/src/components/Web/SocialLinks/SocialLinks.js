f5eb04635bf30f785863698867dd33448b0af529
Daalb <daalbor@uninorte.edu.co>
2020-09-19 22:30:55 -0500

    Working in banner

--- pc-client/src/components/Web/SocialLinks/SocialLinks.js
+++ pc-client/src/components/Web/SocialLinks/SocialLinks.js
@@ -4,6 +4,8 @@ import React from "react";
 
 //Estilos
 import {ReactComponent as FacebookIcon} from "../../../assets/img/svg/facebook.svg";
+import {ReactComponent as InstagramIcon} from "../../../assets/img/svg/instagram.svg";
+
 import "./SocialLinks.scss"
 
 export default function SocialLinks(){
@@ -15,6 +17,14 @@ export default function SocialLinks(){
                 target="_blank"
             >
                 <FacebookIcon/>
+            </a>
+
+            <a 
+                href="https://www.instagram.com/pcuninorte/?hl=es-la"
+                className="instagram"
+                target="_blank"
+            >
+                <InstagramIcon/>
             </a>
         </div>
     )
